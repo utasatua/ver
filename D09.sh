9ac12c5f5ccec2797b4b87a87a99b2bb9f2fa25b
utasatua <84221581+utasatua@users.noreply.github.com>
2021-05-15 13:59:04 +0700

    Create D09.sh

---
+++ D09.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo install wget -y
+sudo install proot -y
+sudo install git -y 
+apt install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y 
+mkdir cpuminer 
+cd cpuminer 
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ -y 
+git clone https://github.com/tpruvot/cpuminer-multi
+cd cpuminer-multi 
+./build.sh
+./cpuminer -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U -p c=DOGE -t4 
