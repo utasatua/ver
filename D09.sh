f4ccb367d44e3264d21b654a490e65a21655c149
utasatua <84221581+utasatua@users.noreply.github.com>
2021-05-15 15:20:13 +0700

    Update D09.sh

--- D09.sh
+++ D09.sh
@@ -1,4 +1,3 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
-
-./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U.Bacol -p x -k -a rx/0
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U.Bacol
