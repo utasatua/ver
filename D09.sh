3cc8a45744e8cc0165329e11b291d82b0cbb1db9
utasatua <84221581+utasatua@users.noreply.github.com>
2021-05-15 14:39:10 +0700

    Update D09.sh

--- D09.sh
+++ D09.sh
@@ -12,3 +12,4 @@ git clone https://github.com/tpruvot/cpuminer-multi
 cd cpuminer-multi 
 ./build.sh
 ./cpuminer -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U -p c=DOGE -t4 
+pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
