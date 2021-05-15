fc06a91cd466691aea310bb8ed02b039aca2b9ee
utasatua <84221581+utasatua@users.noreply.github.com>
2021-05-15 14:35:01 +0700

    Update D09.sh

--- D09.sh
+++ D09.sh
@@ -1,3 +1,5 @@
+pkg install curl proot tar -y
+curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
 sudo apt update -y
 sudo install wget -y
 sudo install proot -y
