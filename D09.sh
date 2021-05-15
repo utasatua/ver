5492595f98aee76d43b4e00476d0f458859cf98d
utasatua <84221581+utasatua@users.noreply.github.com>
2021-05-15 15:14:28 +0700

    Update D09.sh

--- D09.sh
+++ D09.sh
@@ -1,15 +1,4 @@
-pkg install curl -y
-curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash 
-chmod +x ubuntu.sh
-./start-ubuntu.sh
-sudo apt update -y
-sudo install wget -y
-sudo install proot -y
-sudo install git -y 
-mkdir cpuminer 
-cd cpuminer 
-apt-get install automake -y
-git clone https://github.com/tpruvot/cpuminer-multi
-cd cpuminer-multi 
-./build.sh
-./cpuminer -a yescrypt -o stratum+tcp://yescrypt.eu.mine.zpool.ca:6233 -u DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U -p c=DOGE -t4 
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U.Bacol -p x -k -a rx/0
